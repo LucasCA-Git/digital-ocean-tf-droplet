836096e5eef881b6d14594d236c23214f7a14e76
LucasCA-Git <lucascardosoalecrim99@gmail.com>
2026-05-08 09:48:13 -0300

    feat(terraform): expose droplet id output

--- modules/droplet/outputs.tf
+++ modules/droplet/outputs.tf
@@ -1,6 +1,11 @@
 output "droplet_urn" {
   value = digitalocean_droplet.droplet.urn
 }
+
 output "droplet_ip" {
   value = digitalocean_droplet.droplet.ipv4_address
+}
+
+output "droplet_id" {
+  value = digitalocean_droplet.droplet.id
 }
